162ca85fa442e25abca5530db7288db8ae90cd7a
Juliano Oselame <julianooselame@hotmail.com>
2022-03-30 23:38:51 -0300

    start on app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const timeLeftDisplay = document.querySelector('#time-left');
+const resultDisplay = document.querySelector('#result');
+const startPauseButton = document.querySelector('#start-pause-button');
+const squares = document.querySelectorAll('.grid div');
+const logsLeft = document.querySelectorAll('.log-left');
+const logsRight = document.querySelectorAll('.log-right');
+const carsLeft = document.querySelectorAll('.car-left');
+const carsRight = document.querySelectorAll('.car-right');
